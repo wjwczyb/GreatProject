b1522d7cce3c4edf78ba353bb4b62780189867b7
王建伟 <wjwczyb@163.com>
2016-08-15 20:17:16 +0800

    FirstCommit
    20160815

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        $this->display();
+    }
+
+    public function landing(){
+        $this->display();
+    }
+
+    public function sidebar(){
+        $this->display();
+    }
+
+    public function adminindex(){
+        $this->display();
+    }
+
+}
